81ad223db40584a0da11d4515bd1d33eedd42760
MullerJuan <mullerjuanignacio1@gmail.com>
2024-07-18 11:39:51 -0300

    subir repo

--- Basic/02-datatypes.js
+++ Basic/02-datatypes.js
@@ -1,7 +1,7 @@
 // -- Tipos de Datos Primitivos --
 
 // String (cadena de texto)
-let name = "Juani Müller"
+let myName = "Juani Müller"
 let alias = 'Juani'
 let email = `mullerjuanignacio1@gmail.com`
 
@@ -15,9 +15,33 @@ let isTeacher = false
 
 // Undefined (Indefinido)
 let undefinedValue
-console.log(undefinedValue)
 
 //Null (Nulo)
 let nullValue = null
 
 // Symbol (Simbolo)
+let mySymbol = Symbol("mysymbol")
+
+// BigInt (Gran Entero)
+let myBigInt = BigInt(912189121891218912189121891218912189121891218912189121891218)
+let myBigInt2 = 912189121891218912189121891218912189121891218912189121891218n
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof heigth)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
